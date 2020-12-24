c0fed89e73631e47632d5a2361af236c01220662
Stopa42 <honza.stopka@gmail.com>
2020-12-24 13:53:51 +0100

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.13

---
+++ src/HyperoctahedralGroup.jl
@@ -0,0 +1,5 @@
+module HyperoctahedralGroup
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using HyperoctahedralGroup
+using Test
+
+@testset "HyperoctahedralGroup.jl" begin
+    # Write your tests here.
+end
